ff09159359acbef19b734102e3dc5035519ff925
Petru-Vlad Ionescu <ionescupv@gmail.com>
2024-08-12 15:18:47 +0300

    Add new scenarios

--- tests/on_chain_claim_blackbox_test.rs
+++ tests/on_chain_claim_blackbox_test.rs
@@ -10,6 +10,7 @@ const SC_ADDRESS: TestSCAddress =
     TestSCAddress::new("erd1qqqqqqqqqqqqqpgqrhzm5tlnqgyxmc0suqfcfwzn8fxcfdg4dzsqysc3tt");
 const CODE_PATH: MxscPath = MxscPath::new("output/on-chain-claim.mxsc.json");
 const TOKEN: TestTokenIdentifier = TestTokenIdentifier::new("VLAD-6bde05");
+const INVALID_TOKEN: TestTokenIdentifier = TestTokenIdentifier::new("12sasdf");
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
@@ -47,6 +48,202 @@ fn on_chain_claim_blackbox_init() {
     assert_eq!(new_address, SC_ADDRESS);
 
     world.write_scenario_trace("scenarios/trace1.scen.json");
+}
+
+#[test]
+fn on_chain_claim_blackbox_init_invalid_token_id() {
+    let mut world = world();
+    world.start_trace();
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .init(INVALID_TOKEN, 0u64)
+        .code(CODE_PATH)
+        .code_metadata(CodeMetadata::PAYABLE)
+        .returns(ExpectError(4, "Invalid token ID"))
+        .new_address(SC_ADDRESS)
+        .run();
+
+    world.write_scenario_trace("scenarios/trace9.json");
+}
+
+#[test]
+fn on_chain_claim_double_claim() {
+    let mut world = world();
+    world.start_trace();
+
+    let new_address = world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .init(TOKEN, 0u64)
+        .code(CODE_PATH)
+        .code_metadata(CodeMetadata::PAYABLE)
+        .returns(ReturnsNewAddress)
+        .new_address(SC_ADDRESS)
+        .run();
+
+    assert_eq!(new_address, SC_ADDRESS);
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ReturnsResult)
+        .run();
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ExpectError(4, "epoch already claimed"))
+        .run();
+
+    world.write_scenario_trace("scenarios/trace10.json");
+}
+
+#[test]
+fn on_chain_claim_late_claim() {
+    let mut world = world();
+    world.start_trace();
+
+    let new_address = world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .init(TOKEN, 0u64)
+        .code(CODE_PATH)
+        .code_metadata(CodeMetadata::PAYABLE)
+        .returns(ReturnsNewAddress)
+        .new_address(SC_ADDRESS)
+        .run();
+
+    assert_eq!(new_address, SC_ADDRESS);
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ReturnsResult)
+        .run();
+
+    world.current_block().block_epoch(3);
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ReturnsResult)
+        .run();
+
+    let address_info = world
+        .query()
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .get_address_info(OWNER_ADDRESS)
+        .returns(ReturnsResult)
+        .run();
+
+    assert_eq!(address_info.best_streak, 1);
+    assert_eq!(address_info.current_streak, 1);
+    assert_eq!(address_info.last_epoch_claimed, 3);
+    assert_eq!(address_info.total_epochs_claimed, 2);
+
+    world.write_scenario_trace("scenarios/trace11.json");
+}
+
+#[test]
+fn on_chain_claim_wrong_shard_claim() {
+    let mut world = world();
+    world.start_trace();
+
+    let new_address = world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .init(TOKEN, 0u64)
+        .code(CODE_PATH)
+        .code_metadata(CodeMetadata::PAYABLE)
+        .returns(ReturnsNewAddress)
+        .new_address(SC_ADDRESS)
+        .run();
+
+    assert_eq!(new_address, SC_ADDRESS);
+
+    world
+        .tx()
+        .from(SECOND_USER)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ExpectError(4, "wrong shard"))
+        .run();
+
+    world.write_scenario_trace("scenarios/trace12.json");
+}
+
+#[test]
+fn on_chain_claim_claim_happy_path() {
+    let mut world = world();
+    world.start_trace();
+
+    let new_address = world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .init(TOKEN, 0u64)
+        .code(CODE_PATH)
+        .code_metadata(CodeMetadata::PAYABLE)
+        .returns(ReturnsNewAddress)
+        .new_address(SC_ADDRESS)
+        .run();
+
+    assert_eq!(new_address, SC_ADDRESS);
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ReturnsResult)
+        .run();
+
+    world.current_block().block_epoch(2);
+
+    world
+        .tx()
+        .from(OWNER_ADDRESS)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .claim()
+        .returns(ReturnsResult)
+        .run();
+
+    let address_info = world
+        .query()
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .get_address_info(OWNER_ADDRESS)
+        .returns(ReturnsResult)
+        .run();
+
+    assert_eq!(address_info.best_streak, 2);
+    assert_eq!(address_info.current_streak, 2);
+    assert_eq!(address_info.last_epoch_claimed, 2);
+    assert_eq!(address_info.total_epochs_claimed, 2);
+
+    world.write_scenario_trace("scenarios/trace12.json");
 }
 
 #[test]
@@ -306,4 +503,40 @@ fn on_chain_claim_update_state_non_admin() {
         .run();
 
     world.write_scenario_trace("scenarios/trace8.scen.json");
+}
+
+#[test]
+fn on_chain_claim_update_state_wrong_shard() {
+    let mut world = world();
+    world.start_trace();
+
+    let new_address = world
+        .tx()
+        .from(SECOND_USER)
+        .typed(proxy::OnChainClaimContractProxy)
+        .init(TOKEN, 0u64)
+        .code(CODE_PATH)
+        .code_metadata(CodeMetadata::PAYABLE)
+        .returns(ReturnsNewAddress)
+        .new_address(SC_ADDRESS)
+        .run();
+
+    assert_eq!(new_address, SC_ADDRESS);
+
+    world
+        .tx()
+        .from(SECOND_USER)
+        .to(SC_ADDRESS)
+        .typed(proxy::OnChainClaimContractProxy)
+        .update_state(
+            ManagedAddress::from_address(&SECOND_USER.to_address()),
+            1u64,
+            2u64,
+            20u64,
+            40u64,
+        )
+        .returns(ExpectError(4, "wrong shard"))
+        .run();
+
+    world.write_scenario_trace("scenarios/trace13.scen.json");
 }
